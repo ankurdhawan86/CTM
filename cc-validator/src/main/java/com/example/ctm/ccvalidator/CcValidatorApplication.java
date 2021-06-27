c0a8455f702120bdf08f1f26f8aad8d2cac006b4
Ankur Dhawan <dhawan.ankur.86@gmail.com>
2021-06-28 08:47:22 +1000

    First Commit - dummy application

---
+++ cc-validator/src/main/java/com/example/ctm/ccvalidator/CcValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.ctm.ccvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcValidatorApplication.class, args);
+	}
+
+}
---
+++ cc-validator/src/test/java/com/example/ctm/ccvalidator/CcValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ctm.ccvalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
